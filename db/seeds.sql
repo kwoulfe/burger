0a82f53f152e25698483ac21bc721a278e54db45
Kevin Woulfe <kevinpwoulfe@gmail.com>
2018-04-30 01:17:40 -0700

    Basic Setup Complete

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Big Kahuna Burger');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Royal w/ Cheese');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Le Big Mac');
