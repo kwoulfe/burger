2fac526ecec15a6da60578aac042cec5a81fca13
Kevin Woulfe <kevinpwoulfe@gmail.com>
2018-05-02 15:03:31 -0700

    Working files

--- models/burger.js
+++ models/burger.js
@@ -1,17 +1,17 @@
 var orm = require('../config/orm.js');
 
 var burger = {
-  selectAll: function(cb) {
+  all: function(cb) {
     orm.all('burgers', function(res) {
       cb(res);
     });
   },
-  insertOne: function(cols, vals, cb) {
+  create: function(cols, vals, cb) {
     orm.create('burgers', cols, vals, function(res) {
       cb(res);
     });
   },
-  updateOne: function(objColVals, condition, cb) {
+  update: function(objColVals, condition, cb) {
     orm.update('burgers', objColVals, condition, function(res) {
       cb(res);
     });
